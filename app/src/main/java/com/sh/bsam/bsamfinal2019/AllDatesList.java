7b9c5d52ef39df1973144b4b776306cfbf224ab7
Owner <alsalamcse>
2019-03-20 08:39:09 +0200

    sign in|up is good .. DATE PICKER DONE.. ..

--- app/src/main/java/com/sh/bsam/bsamfinal2019/AllDatesList.java
+++ app/src/main/java/com/sh/bsam/bsamfinal2019/AllDatesList.java
@@ -1,7 +1,10 @@
 package com.sh.bsam.bsamfinal2019;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -19,6 +22,18 @@ public class AllDatesList extends AppCompatActivity {
         setContentView(R.layout.activity_all_dates_list);
 
         listL=(ListView)findViewById(R.id.listL);
+
+        ArrayAdapter<String>adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_2,android.R.id.text1,listviewitems);
+        listL.setAdapter(adapter);
+        listL.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String Templistview=listviewitems[position].toString();
+                Intent intent=new Intent(AllDatesList.this,informationActivity.class);
+                intent.putExtra("Listviewclickvalue",Templistview);
+                startActivity(intent);
+            }
+        });
 
 
 
--- app/src/main/java/com/sh/bsam/bsamfinal2019/logActivity.java
+++ app/src/main/java/com/sh/bsam/bsamfinal2019/logActivity.java
@@ -64,7 +64,7 @@ public class logActivity extends AppCompatActivity {
         INTENT.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), PickDateActivity.class);
+                Intent intent = new Intent(getApplicationContext(), AllDatesList.class);
                 startActivity(intent);
 
             }
